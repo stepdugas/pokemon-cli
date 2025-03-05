615d6fce4cd1854542cbd1def24478f6193acc55
Stephanie <stepdugas@gmail.com>
2025-03-05 11:16:26 -0500

    Committing classes

---
+++ src/main/java/org/example/models/Results.java
@@ -0,0 +1,17 @@
+package org.example.models;
+
+import java.util.List;
+
+public class Results {
+
+    List<Pokemon> results;
+
+    public List<Pokemon> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Pokemon> results) {
+        this.results = results;
+    }
+
+}
