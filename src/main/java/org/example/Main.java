82c70211aed13b4a4861421a2ecbb7952e7d9225
Stephanie <stepdugas@gmail.com>
2025-03-05 11:38:59 -0500

    Committing updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,21 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.services.PokemonService;
+
+import java.util.Scanner;
+
+
 public class Main {
+
+    private static PokemonService service = new PokemonService();
+    private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.println("Welcome Pokemon Trainer!");
+        System.out.println("1. Get first 20");
+        System.out.println("2. Get second 20");
+        System.out.println("3. Get next 20");
 
 
     }
--- src/main/java/org/example/services/PokemonService.java
+++ src/main/java/org/example/services/PokemonService.java
@@ -1,5 +1,17 @@
 package org.example.services;
+import org.example.models.Pokemon;
+import org.example.models.Results;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.util.*;
+import java.util.List;
 
 public class PokemonService {
+    private RestTemplate restTemplate = new RestTemplate();
+    private final String API_URL="https://pokeapi.co/api/v2/pokemon/";
+    public List<Pokemon> getPokemon(){
+        Results rs = restTemplate.getForObject(API_URL, Results.class);
+        return rs.getResults();
+    }
+
 
 }
